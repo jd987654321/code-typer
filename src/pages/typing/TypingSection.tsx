a238722f92b71890e4114802be31dd892a02bfd7
jd987654321 <jacobdam06@gmail.com>
2025-07-10 01:01:28 -0400

    refactored text rendering to be able to display current word as white

--- src/pages/typing/TypingSection.tsx
+++ src/pages/typing/TypingSection.tsx
@@ -1,5 +1,5 @@
 /**
- * CUSTOM RENDERING NOTES
+ * ? CUSTOM RENDERING NOTES
  * split the entire div into individual spans and words
  * so we want the input to be line by line so when we hit the enter we should clear the entire thing
  *
@@ -12,7 +12,7 @@
  */
 
 /**
- * WPM NOTES
+ * ? WPM NOTES
  *
  * how tf does our text rendering work here, so textArr holds a true or false for each word
  * i think the word in the input box gets matched to the letters here
@@ -24,12 +24,19 @@
  */
 
 /**
- * INTEGRATING SHIKI WITH OUR CUSTOM RENDERING NOTES
+ * ? INTEGRATING SHIKI WITH OUR CUSTOM RENDERING NOTES
  *
  * so shiki creates the html blocks that we then insert, we can also try custom rendering where we render it ourselves
  * I think we grab what we need from shiki, but manually render the last word, maybe split the text string into two
  * the one with highlighting, and the stuff without highlighting, we are pretty much rerendering a majority of the text each time someone types anything
- *if this is too slow then we can optimize by choosing what part of the text we render
+ *  if this is too slow then we can optimize by choosing what part of the text we render
+ *
+ * so we use the bitty value to keep track of which word we are at, if we are currently larger then the word variable
+ * we can stick with space checking if the word is the same value, but we can also just check
+ * each time we type if the word is the same, but then, the tricky part is how do we differentiate?
+ *We can have a seperate array which dictates whether or not a space is needed to get to the next part, 
+ custom function which goes thru the text string and finds the corresponding word, and looks at the
+ punctuation afterwards.
  */
 
 import { useState, useRef, useEffect, ReactElement, useMemo } from "react";
@@ -67,6 +74,9 @@ export default function TypingSection({
 
   const typeText3: string =
     'import { ReactElement } from "react";\n\ntype Props = {\n  recordedSpeed: boolean[];\n};\n\nexport default function FinishedSection({\n  recordedSpeed,\n}: Props): ReactElement {\n  console.log(recordedSpeed);\n  const [hok, setHok] = useState<number>(0);\n  return (\n    <div>\n      <p>Nice job ur done now :D</p>\n      <p>Result: {recordedSpeed.filter(Boolean).length} WPM</p>\n    </div>\n  );\n}\n';
+  //import goes from index 0 to 5 import.length is 6 so from the current index we would need to go word.length-1
+  //if i is the index of the first letter of our word, then we are looking at index i+word.length
+  //but first we should do a while loop to match the first letter
 
   //const textArr: string[] = typeText.split(" ");
   const textArr: string[] = typeText.split(/[\n\s]+/);
@@ -87,6 +97,7 @@ export default function TypingSection({
   const textArr2: string[] = useMemo(() => {
     let arr: string[] = [];
     /**
+     * ? What this parse function is doing
      * an html element such as <div>some text</div> actually counts as two nodes, since the text
      * is actually a text node, and this text node counts as a child node of the <div></div> node
      * So when checking the number of children each non-nested node should have, its's actually one, not zero
@@ -105,12 +116,29 @@ export default function TypingSection({
           //console.log(node.name);
           let text: string[] = getTextFromNode(node).trim().split(/\s+/);
           text.forEach((item) => arr.push(item));
+          //arr.push(getTextFromNode(node));
           return node;
         }
       },
     });
     return arr;
   }, []);
+
+  let afterResults: string[] = [];
+
+  let p: number = 0;
+  for (const word of textArr2) {
+    while (word[0] !== typeText3[p]) {
+      p++;
+    }
+    if (typeText3[p + word.length] === " ") {
+      afterResults.push("space");
+    } else if (typeText3[p + word.length] === "\n") {
+      afterResults.push("newline");
+    } else {
+      afterResults.push("something");
+    }
+  }
 
   useEffect(() => {
     const focusText = () => {
@@ -196,31 +224,58 @@ export default function TypingSection({
         );
       }
 
+      //? node is an html element, x extracts the exact # rgb hexcode value from the element
+      //there is only one hexcode thus, we use x[0] down below in the code
+
       const x = n ? n.match(/#([0-9a-fA-F]{6})\b/) : null;
       if (node.children?.length === 1 && node.name === "span" && x != null) {
-        let isGray: boolean = bitty > word;
-        //console.log("ran");
-        bitty++;
-        //console.log(node.attribs?.style);
-        const split = getTextFromNode(node).match(/ *\S+ */g) || [];
+        const splitt = getTextFromNode(node).match(/ *\S+ */g) || [];
+        return (
+          <>
+            {splitt.map((item, index) => {
+              bitty++;
+              let isGray: boolean = bitty >= word;
 
-        if (isGray) {
-          return (
-            <>
-              {split.map((item, index) => (
-                <span style={{ color: "#333333" }}>{item}</span>
-              ))}
-            </>
-          );
-        } else {
-          return (
-            <>
-              {split.map((item, index) => (
-                <span style={{ color: x[0] }}>{item}</span>
-              ))}
-            </>
-          );
-        }
+              //if bitty is equal to word, then we split up the letters in the word,
+              //the way we've split everything up, each thing should be by itself or will have spaces
+              //so we can split it into space, word, space
+              //then
+              return (
+                <span
+                  key={item + index}
+                  style={{ color: isGray ? "#333333" : x[0] }}
+                >
+                  {/* item is a string */}
+                  {bitty === word
+                    ? item.match(/(\s+|[^\s]+)/g)?.map((thing, index) => {
+                        if (!/^\s*$/.test(thing)) {
+                          return (
+                            <>
+                              {thing.split("").map((thang, index) => {
+                                if (userText[index] === thang) {
+                                  return (
+                                    <span style={{ color: "#FFFFFF" }}>
+                                      {thang}
+                                    </span>
+                                  );
+                                }
+                                return (
+                                  <span style={{ color: "#333333" }}>
+                                    {thang}
+                                  </span>
+                                );
+                              })}
+                            </>
+                          );
+                        }
+                        return <span>{thing}</span>;
+                      })
+                    : item}
+                </span>
+              );
+            })}
+          </>
+        );
       }
 
       return node;
@@ -232,12 +287,20 @@ export default function TypingSection({
     replace: replace,
   });
 
+  //console.log(typeText3);
+  // console.log(textArr2.length);
+  // console.log(afterResults.length);
+  //console.log(hlText);
+
   console.log(textArr2);
-  console.log(hlText);
+  //console.log(word);
 
   return (
     <>
-      <div className="w-[90%]  bg-none">
+      <div className="w-[90%] text-white bg-none">
+        <button onClick={() => setWord((val) => val + 1)}>+1</button>
+        <button onClick={() => setWord((val) => val - 1)}>-1</button>
+        <div>{userText}</div>
         {/* <div
           className="text-sm font-vscodeTitle"
           dangerouslySetInnerHTML={{ __html: testShiki }}
@@ -248,6 +311,8 @@ export default function TypingSection({
           {/* textArr is the string[] holding all the actual words, textState is the boolean[] telling us if 
           each of the words have been correctly typed out */}
         </p>
+
+        <div>{userText}</div>
 
         <input
           type="text"
@@ -260,21 +325,14 @@ export default function TypingSection({
               //console.log(e.target.value);
               setUserText(e.target.value);
 
+              //* New Notes with shiki highlighting
+              // For now I think everytime we type, shiki should redo everything,
+              //so if word index matches the bitty variable (what we use to keep track of the word count in the code snippet)
+              //then we will match the number of words to it
+
+              //! Old Notes
               //everytime space is clicked, we check if the current word in this input element is the same
               //as the word we are at in the textArr as index word (the index variable is literally "word")
-              if (
-                keyPressed === " " &&
-                textArr[word] === e.target.value.trim()
-              ) {
-                //console.log("same");
-                setTextStates((textStates) =>
-                  textStates.map((item, index) =>
-                    index === word ? true : textStates[index]
-                  )
-                );
-                setWord((word) => word + 1);
-                setUserText("");
-              }
             }
           }}
           onKeyDown={(e) => {
